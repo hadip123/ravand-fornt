f9ee1b674869a63ce1b94271ce47ad1ffa3a3f4a
Mohammad Hadi <mhn.hadi@outlook.com>
2023-03-18 20:13:10 +0330

    init

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:taskify/start/start_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Start(),
+  ));
+}
---
+++ lib/start/start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Start extends StatefulWidget {
+  const Start({super.key});
+
+  @override
+  State<Start> createState() => _StartState();
+}
+
+class _StartState extends State<Start> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
